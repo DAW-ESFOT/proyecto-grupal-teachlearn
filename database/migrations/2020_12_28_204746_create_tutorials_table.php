8862f000bc62c52dcf3d8fac2b6b0776b35eaeee
Jona141999 <jona94alquinga@gmail.com>
2020-12-28 21:32:11 -0500

    #13 modelo tutorias

---
+++ database/migrations/2020_12_28_204746_create_tutorials_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTutorialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tutorials', function (Blueprint $table) {
+            $table->bigIncrements('id_tutorial');
+            $table->dateTime('date_and_hour');
+            $table->float('pay_values');
+            $table->string('image');
+            $table->time('duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tutorials');
+    }
+}
